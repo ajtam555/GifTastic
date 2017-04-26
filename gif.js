63d0831722de4bc7c41cf3db8b5e1d0fac3b2909
Alex Tam <AJT@Alexanders-MacBook-Pro.local>
2017-04-25 21:26:16 -0700

    js psuedo comments

---
+++ gif.js
@@ -0,0 +1,29 @@
+var gif = {
+
+	// Initializer function
+		// Array
+		// Loop array
+
+	// Render buttons function
+		// Only add new array location to display
+
+	// Text input function
+		// Create button
+		// Append button
+		// Push into array
+		// Call Render buttons function
+
+	// API call function
+		// Call API function for any buttons clicked
+
+	// Clear gifs function
+		// Call clear function for any button clicked
+
+
+
+
+}
+
+$(document).ready(function(){
+
+});
